6bc380efeee8127045b5cb2764ddcdb85661ba7e
snacksgg <snacksgg@users.noreply.github.com>
2025-12-05 01:57:54 -0600

    h

--- docs/js/lightgallery.umd.js
+++ docs/js/lightgallery.umd.js
@@ -155,7 +155,7 @@
             nextSlide: 'Next slide',
             download: 'Download',
             playVideo: 'Play video',
-            mediaLoadingFailed: 'Image Not Found',
+            mediaLoadingFailed: '--------------',
         },
     };
 
